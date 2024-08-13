71c8a36edf128118f464db4c405c3c9c0f597d79
Kingdk88 <143623116+Kingdk88@users.noreply.github.com>
2024-08-13 11:26:41 +0530

    Update config.py
    Signed-off-by: Kingdk88 <143623116+Kingdk88@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Deepking88/AnonXMusic",
+    "https://github.com/kingdk88/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
